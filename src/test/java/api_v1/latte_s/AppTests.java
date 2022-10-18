454460684772468b7db63141034ea20c56728a72
Tuan Trinh <tuan882612@gmail.com>
2022-10-18 06:35:21 -0500

    Initial commit

---
+++ src/test/java/api_v1/latte_s/AppTests.java
@@ -0,0 +1,13 @@
+package api_v1.latte_s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
